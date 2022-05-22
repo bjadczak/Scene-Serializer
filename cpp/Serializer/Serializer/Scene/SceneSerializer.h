fdba5b4e2b905a2c262e30b697379a1c57b6fc49
Paweł Dębski <pawel.debski.99@gmail.com>
2022-05-22 18:28:01 +0200

    Added validation on creating PointRefs

--- cpp/Serializer/Serializer/Scene/SceneSerializer.h
+++ cpp/Serializer/Serializer/Scene/SceneSerializer.h
@@ -1,18 +1,11 @@
 #pragma once
 
 #include <filesystem>
-#include <exception>
 
 #include "Scene.h"
 
 namespace MG1
 {
-    //std:exception nie powinno miec konstruktora z const char* (rozszerzenie MSVC)
-	struct SerializerException : public std::runtime_error
-	{
-		SerializerException(const char* message) : std::runtime_error(message) { }
-	};
-
 	class SceneSerializer
 	{
 	public:
